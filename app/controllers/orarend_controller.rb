6fb559966d8e2574de6485498f1837342565ecff
Daniel Kiss <daniel.mark.kiss@gmail.com>
2023-05-05 17:22:21 +0200

    feat: :sparkles: orarend init

--- app/controllers/orarend_controller.rb
+++ app/controllers/orarend_controller.rb
@@ -2,5 +2,6 @@ class OrarendController < ApplicationController
     def get
       print("orarend get\n\n\n")
       @osztaly = "7/b"
+      orarend = OrarendModell.new('igenn')
     end
 end
--- app/models/orarend_modell.rb
+++ app/models/orarend_modell.rb
@@ -1,2 +1,5 @@
 class OrarendModell < ApplicationRecord
+    def initialize(comment)
+        print("Orarend_init\n")
+    end
 end
